f27bb49dfbb0913dd4492e1393c40004d65d7a0b
DanHam <DanHam@users.noreply.github.com>
2016-06-08 21:17:46 +0100

    Import CentOS RPM GPG key to prevent output of warnings on update

--- centos/scripts/00-yum-update.sh
+++ centos/scripts/00-yum-update.sh
@@ -2,6 +2,12 @@
 #
 # Update installed packages and initiate system restart as required
 
+# Path to CentOS 7 RPM GPG signing key
+RPM_GPG_KEY="/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7"
+# CentOS 7 Official Signing Key ID
+RPM_GPG_KEY_ID="f4a80eb5"
+# CentOS 7 Official Signing Key Fingerprint
+RPM_GPG_KEY_FINGER="6341 AB27 53D7 8A78 A7C2  7BB1 24C6 A8A7 F4A8 0EB5"
 # Set the location for storing the list of updates from yum check-update
 UPDATE_FILE="/tmp/updates.txt"
 # Create a grep friendly list of packages that require a system reboot
@@ -10,6 +16,21 @@ UPDATE_FILE="/tmp/updates.txt"
 REBOOT_LIST="-e ^kernel -e ^glibc -e ^linux-firmware -e ^systemd -e ^udev"
 # Initialise Reboot flag
 REBOOT=0
+
+
+# Ensure the CentOS RPM GPG key has been imported
+if [ "x$(rpm -qa gpg-pubkey* | grep $RPM_GPG_KEY_ID)" = "x" ]; then
+    # Check the finger print of the key matches the known value
+    FINGER="$(gpg -q --with-fingerprint $RPM_GPG_KEY |\
+              grep fingerprint | \
+              sed -n -e 's/^.*= //p')"
+    if [ "$FINGER" == "$RPM_GPG_KEY_FINGER" ]; then
+        rpm --import $RPM_GPG_KEY &> /dev/null
+    else
+        echo "ERROR: CentOS RPM GPG key does not match known fingerprint"
+        exit -1
+    fi
+fi
 
 
 # Check if updates are required and store the list of update packages
@@ -37,10 +58,12 @@ else
     echo "No packages on the system require updating."
 fi
 
+
 # Remove cached packages and metadata from the yum repository
 yum clean all > /dev/null
 # Remove temp files
 [[ -e $UPDATE_FILE ]] && rm -f $UPDATE_FILE
+
 
 # Reboot if required
 if [ $REBOOT == 1 ]; then
