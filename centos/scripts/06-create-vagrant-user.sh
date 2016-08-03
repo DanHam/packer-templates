a17511b8aebedf7eff18a10bb9fd29de7cbf97c7
DanHam <DanHam@users.noreply.github.com>
2016-08-03 10:00:17 +0100

    Use generic name "admin" rather than "vagrant" for template and script vars

--- centos/scripts/06-create-vagrant-user.sh
+++ centos/scripts/06-create-vagrant-user.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
-# Create a user for Vagrant and configure. Required values are set in the
-# packer configuration file and injected into the build environment as
+# Create a user and configure. Required values are set in the packer
+# configuration file and injected into the build environment as
 # environment variables. Note that we do not set a password and as such
 # password based authentication is disabled. Additionally note ssh has
 # been configured to only allow authentication via ssh keys.
@@ -10,45 +10,46 @@
 [[ "$DEBUG" = true ]] && REDIRECT="/dev/stdout" || REDIRECT="/dev/null"
 
 # Packer logging
-echo "Creating user for Vagrant and setting up required options..."
+echo "Creating an admin user and setting up required options..."
 
-echo "User:      ${VAGRANT_USER}"               > ${REDIRECT}
-echo "Group:     ${VAGRANT_GROUP}"              > ${REDIRECT}
-echo "UID:       ${VAGRANT_UID}"                > ${REDIRECT}
-echo "GID:       ${VAGRANT_GID}"                > ${REDIRECT}
-echo "Groups:    ${VAGRANT_GROUPS}"             > ${REDIRECT}
-echo "Shell:     ${VAGRANT_SHELL}"              > ${REDIRECT}
-echo "GECOS:     ${VAGRANT_GECOS}"              > ${REDIRECT}
-echo "SSH key:   ${VAGRANT_SSH_AUTHORISED_KEY}" > ${REDIRECT}
+echo "User:      ${ADMIN_USER}"               > ${REDIRECT}
+echo "Group:     ${ADMIN_GROUP}"              > ${REDIRECT}
+echo "UID:       ${ADMIN_UID}"                > ${REDIRECT}
+echo "GID:       ${ADMIN_GID}"                > ${REDIRECT}
+echo "Groups:    ${ADMIN_GROUPS}"             > ${REDIRECT}
+echo "Shell:     ${ADMIN_SHELL}"              > ${REDIRECT}
+echo "GECOS:     ${ADMIN_GECOS}"              > ${REDIRECT}
+echo "SSH key:   ${ADMIN_SSH_AUTHORISED_KEY}" > ${REDIRECT}
 
 # Create the required group
-groupadd --gid ${VAGRANT_GID} ${VAGRANT_GROUP}
+groupadd --gid ${ADMIN_GID} ${ADMIN_GROUP}
 
 # Create the user
-useradd --create-home --uid ${VAGRANT_UID} --gid ${VAGRANT_GID} \
-        --groups ${VAGRANT_GROUPS} --shell ${VAGRANT_SHELL} \
-        --comment "${VAGRANT_GECOS}" ${VAGRANT_USER}
+useradd --create-home --uid ${ADMIN_UID} --gid ${ADMIN_GID} \
+        --groups ${ADMIN_GROUPS} --shell ${ADMIN_SHELL} \
+        --comment "${ADMIN_GECOS}" ${ADMIN_USER}
 
-# Configure authorised ssh keys for Vagrant
-SSH_DIR="/home/${VAGRANT_USER}/.ssh"
+# Configure authorised ssh keys
+SSH_DIR="/home/${ADMIN_USER}/.ssh"
 [[ -d ${SSH_DIR} ]] || mkdir ${SSH_DIR}
 chmod 700 ${SSH_DIR}
-echo ${VAGRANT_SSH_AUTHORISED_KEY} > ${SSH_DIR}/authorized_keys
+echo ${ADMIN_SSH_AUTHORISED_KEY} > ${SSH_DIR}/authorized_keys
 chmod 600 ${SSH_DIR}/authorized_keys
-chown -R ${VAGRANT_USER}:${VAGRANT_GROUP} ${SSH_DIR}
+chown -R ${ADMIN_USER}:${ADMIN_GROUP} ${SSH_DIR}
 
 # If set disable the requirement for a TTY with sudo see:
 # https://bugzilla.redhat.com/show_bug.cgi?id=1020147
+# This is a requirement when configuring for use with Vagrant
 sed -i '/^Defaults .*requiretty/ s/^/# /g' /etc/sudoers
 
-# Configure password-less sudo for Vagrant user
+# Configure password-less sudo for the admin user
 SUDOERS_USER="/etc/sudoers.d/admin-user"
 cat <<EOF > ${SUDOERS_USER}
 # Allow admin user to run commands as root without providing a password
-${VAGRANT_USER}  ALL=(ALL)  NOPASSWD: ALL
+${ADMIN_USER}  ALL=(ALL)  NOPASSWD: ALL
 # Disable the requirement for a TTY with sudo see:
 # https://bugzilla.redhat.com/show_bug.cgi?id=1020147
-Defaults:${VAGRANT_USER} !requiretty
+Defaults:${ADMIN_USER} !requiretty
 EOF
 chmod 0440 ${SUDOERS_USER}
 
