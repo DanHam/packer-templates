125e6a73f187eb96560044a86c1b44af190260a7
DanHam <DanHam@users.noreply.github.com>
2016-09-28 18:41:18 +0100

    Use NTP source for system time when applicable - This is not Debian default

--- centos/scripts/06-configure-timedate-settings.sh
+++ centos/scripts/06-configure-timedate-settings.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 #
-# Disable unsupported reading of time from the RTC in the local time zone
+# Configure system time and date settings
+
+# Set verbose/quiet output based on env var configured in Packer template
+[[ "$DEBUG" = true ]] && REDIRECT="/dev/stdout" || REDIRECT="/dev/null"
 
 # Packer logging
-echo "Disabling reading of time from RTC in local time zone..."
+echo "Configuring system time and date settings..."
 
-timedatectl set-local-rtc 0
+# Service Daemons
+CHRONYD="chronyd.service"
+NTPD="ntpd.service"
+
+# Configure system date time to be set from Network time source when
+# the chronyd or ntpd service is enabled
+if [[ "$(systemctl is-enabled ${CHRONYD} 2>/dev/null)" == "enabled" || \
+      "$(systemctl is-enabled ${NTPD} 2>/dev/null)" == "enabled" ]]; then
+    echo "System time configured to use NTP based source" > ${REDIRECT}
+    timedatectl set-ntp true
+else
+    echo "System time will not use an NTP based source" > ${REDIRECT}
+    timedatectl set-ntp false
+fi
+
+# Disable unsupported reading of time from the RTC in the local time zone
+echo "Disabling system setting of the RTC" > ${REDIRECT}
+timedatectl set-local-rtc false
 
 exit 0
--- debian/scripts/05-configure-timedate-settings.sh
+++ debian/scripts/05-configure-timedate-settings.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 #
-# Disable unsupported reading of time from the RTC in the local time zone
+# Configure system time and date settings
+
+# Set verbose/quiet output based on env var configured in Packer template
+[[ "$DEBUG" = true ]] && REDIRECT="/dev/stdout" || REDIRECT="/dev/null"
 
 # Packer logging
-echo "Disabling reading of time from RTC in local time zone..."
+echo "Configuring system time and date settings..."
 
-timedatectl set-local-rtc 0
+# Configure system date time to be set from Network time source when
+# the chronyd or ntpd service is enabled
+# Note that Debians init scripts for ntp and chrony haven't yet been
+# ported over to systemd style unit files so we need to check for startup
+# scripts for each run level
+if [ "x$(find /etc/rc?.d -name S??ntp -o -name S??chrony)" != "x" ]; then
+    echo "System time configured to use NTP based source" > ${REDIRECT}
+    timedatectl set-ntp true
+else
+    echo "System time will not use an NTP based source" > ${REDIRECT}
+    timedatectl set-ntp false
+fi
+
+# Disable unsupported reading of time from the RTC in the local time zone
+echo "Disabling system setting of the RTC" > ${REDIRECT}
+timedatectl set-local-rtc false
 
 exit 0
