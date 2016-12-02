88e8e4cf289d454568dd036d9fce7b04ce8cd0bd
DanHam <DanHam@users.noreply.github.com>
2016-12-02 11:10:25 +0000

    Use 'attach' method for VBox Additions due to sporadic WinRM 500 error #4203

--- windows2012r2/files/InstallGuestTools.ps1
+++ windows2012r2/files/InstallGuestTools.ps1
@@ -6,49 +6,100 @@ $vboxga_iso = $env:VBOXGA_ISO_PATH
 $vmware_iso = $env:VMTOOLS_ISO_PATH
 
 # Enumerate the platform we are on
-$virt_platform = (Get-WmiObject -Class Win32_ComputerSystem).Manufacturer
+$platform = (Get-WmiObject -Class Win32_ComputerSystem).Manufacturer
 
-if (($virt_platform -like '*innotek*') -and (Test-Path -Path $vboxga_iso))
-{
-    Write-Host 'Virtualbox platform detected and Guest Additions ISO found'
+if ($platform -like '*innotek*') {
+    Write-Host 'Virtualbox platform detected'
 
-    # Install the Oracle certificate so the vbox additions are trusted
-    Write-Host '* Adding Oracle CA cert to Trusted Publisher store'
-    certutil -addstore -f 'TrustedPublisher' A:\oracle.cer | Out-Null
+    # Find the Guest Additions Installer
+    [bool]$vboxga_installer_found = $false
 
-    # Mount the Guest Additions ISO and run the installer
-    Write-Host '* Mounting the ISO and installing VirtualBox Guest Additions'
-    Mount-DiskImage -ImagePath $vboxga_iso
-    $drive = (Get-DiskImage -ImagePath $vboxga_iso | Get-Volume).Driveletter
-    $installer = $drive + ':\VBoxWindowsAdditions.exe'
-    $parameters = '/S'
-    Start-Process $installer $parameters -Wait
+    # Guest Additions can be uploaded or 'attached' via the optical drive
+    if ($vboxga_iso -ne $null) {
+        if (Test-Path -Path $vboxga_iso) {
+            # Guest Additions were uploaded
+            Write-Host "* Guest Additions ISO found at $vboxga_iso"
+            # Mount the Guest Additions ISO
+            Write-Host '* Mounting the ISO'
+            Mount-DiskImage -ImagePath $vboxga_iso
+        }
+    }
 
-    # Dismount and delete the ISO
-    Dismount-DiskImage -ImagePath $vboxga_iso
-    Write-Host '* Deleting uploaded guest additions ISO'
-    Remove-Item $vboxga_iso
-    Write-Host 'Complete'
+    # If we have found and mounted the ISO or if we are checking whether
+    # the Guest Additions have been 'attached' the same method can be
+    # used to enumerate the drive letter and root and subsequently set the
+    # path to the installer.
+    # If we have found and mounted the ISO but fail to find the installer
+    # it's possible the ISO is corrupt. Otherwise it's possible the ISO
+    # has not been attached. It could also be possible in either case that
+    # the name of the installer has been changed
+    Get-Volume | % {
+        if ( $($_.FileSystemLabel) -like 'VBOXADDITIONS*' ) {
+            Write-Host "* Guest Additions in Volume $($_.DriveLetter)"
+            $root = (Get-PSDrive -Name $($_.DriveLetter)).Root
+            # Set the full path to the installer
+            $installer = $root + 'VBoxWindowsAdditions.exe'
+            if (Test-Path -Path $installer) {
+                [bool]$vboxga_installer_found = $true
+            }
+        }
+    }
+
+    # Install the Guest Additions
+    if ($vboxga_installer_found) {
+        # Install the Oracle certificate so the vbox additions are trusted
+        Write-Host '* Adding Oracle CA cert to Trusted Publisher store'
+        certutil -addstore -f 'TrustedPublisher' A:\oracle.cer | Out-Null
+        Write-Host '* Installing the Guest Additions'
+        $parameters = '/S'
+        Start-Process $installer $parameters -Wait
+    } else {
+        Write-Host '* WARNING: Failed to find Guest Additions installer'
+    }
+
+    # Regardless of whether the installer was actually found, If the Guest
+    # Additions were uploaded and mounted we need to tidy up
+    if ($vboxga_iso -ne $null) {
+        if (Test-Path -Path $vboxga_iso) {
+            if ( (Get-DiskImage -ImagePath $vboxga_iso).Attached ) {
+                Write-Host '* Unmounting the Guest Additions ISO'
+                Dismount-DiskImage -ImagePath $vboxga_iso
+            }
+            Write-Host '* Deleting uploaded Guest Additions ISO'
+            Remove-Item $vboxga_iso
+        }
+    }
+
+} elseif ($platform -like '*VMware*') {
+
+    Write-Host 'VMware platform detected'
+
+    if ($vmware_iso -ne $null) {
+        if (Test-Path -Path $vmware_iso) {
+
+            Write-Host "* VMware Tools ISO found at $vmware_iso"
+
+            # Mount the VMware tools ISO and run the installer
+            Write-Host '* Mounting the ISO and installing VMware Tools'
+            Mount-DiskImage -ImagePath $vmware_iso
+            $drive = (Get-DiskImage -ImagePath $vmware_iso | Get-Volume).Driveletter
+            $root = (Get-PSDrive -Name $drive).Root
+            $installer = $root + 'setup64.exe'
+            $parameters = '/S /v "/qn REBOOT=R"'
+            Start-Process $installer $parameters -Wait
+
+            # Dismount and delete the ISO
+            Dismount-DiskImage -ImagePath $vmware_iso
+            Write-Host '* Deleting uploaded VMware tools ISO'
+            Remove-Item $vmware_iso
+        } else {
+            Write-Host "* WARNING: VMware tools ISO not found: $vmware_iso"
+        }
+    } else {
+        Write-Host "* WARNING: Path to uploaded ISO unknown: empty env var"
+    }
+} else {
+    Write-Host "* WARNING: Unknown platform: $platform"
 }
-elseif (($virt_platform -like '*VMware*') -and (Test-Path -Path $vmware_iso))
-{
-    Write-Host 'VMware platform detected and VMware Tools ISO found'
 
-    # Mount the VMware tools ISO and run the installer
-    Write-Host '* Mounting the ISO and installing VMware Tools'
-    Mount-DiskImage -ImagePath $vmware_iso
-    $drive = (Get-DiskImage -ImagePath $vmware_iso | Get-Volume).Driveletter
-    $installer = $drive + ':\setup64.exe'
-    $parameters = '/S /v "/qn REBOOT=R"'
-    Start-Process $installer $parameters -Wait
-
-    # Dismount and delete the ISO
-    Dismount-DiskImage -ImagePath $vmware_iso
-    Write-Host '* Deleting uploaded VMware tools ISO'
-    Remove-Item $vmware_iso
-    Write-Host 'Complete'
-}
-else
-{
-    Write-Host "WARNING: Virt platform not detected or no guest tools found"
-}
+Write-Host 'Complete'
