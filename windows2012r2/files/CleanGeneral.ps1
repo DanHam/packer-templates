280526c681f528372429fe1f8f08cd4819e04166
DanHam <DanHam@users.noreply.github.com>
2016-12-17 14:07:52 +0000

    Clean downloaded update files from %windir%/SoftwareDistribution/Downloads/*

--- windows2012r2/files/CleanGeneral.ps1
+++ windows2012r2/files/CleanGeneral.ps1
@@ -11,12 +11,20 @@ If ( $env:CLEAN_GENERAL -ne $true ) {
 
 Write-Host 'Performing general clean up of Windows as requested'
 
+# Cleaning Windows Update files requires we stop the Windows Updates
+# service
+If ( (Get-Service wuauserv).Status -eq 'Running') {
+    Write-Host '* Cleaning Windows Updates downloads so stopping Windows Update service'
+    Stop-Service wuauserv -Force
+    [bool]$WindowsUpdatesRunning = $true
+}
+
 # Cleaning the Windows Component store ManifestCache requires we shutdown
 # the Trusted Installer service
 If ( (Get-Service TrustedInstaller).Status -eq 'Running') {
     Write-Host '* Cleaning WinSxS ManifestCache so stopping Trusted Installer service'
     Stop-Service TrustedInstaller
-    $TrustedInstallerRunning = $true
+    [bool]$TrustedInstallerRunning = $true
 }
 
 # Loop through items in the array and remove
@@ -26,6 +34,7 @@ If ( (Get-Service TrustedInstaller).Status -eq 'Running') {
     "$env:windir\Panther\*",
     "$env:windir\Temp\*",
     "$env:windir\WinSxS\ManifestCache\*"
+    "$env:windir\SoftwareDistribution\Download\*"
 ) | % {
     if(Test-Path $_) {
         Write-Host "* Removing $_"
@@ -35,6 +44,12 @@ If ( (Get-Service TrustedInstaller).Status -eq 'Running') {
             Remove-Item $_ -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
         } catch { $global:error.RemoveAt(0) }
     }
+}
+
+# Restart the Windows Updates service if it was previously running
+if ( $WindowsUpdatesRunning ) {
+    Write-Host '* Restarting Windows Updates service post clean'
+    Start-Service wuauserv
 }
 
 # Restart the Trusted Installer service if it was previously running
