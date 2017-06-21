4220bd60ddabbc38d365c0d5c82b5cbb67e0a03a
DanHam <DanHam@users.noreply.github.com>
2017-06-21 17:23:40 +0100

    Updates for Debian 9.0 build
    * open-vm-tools with the required fix for shared folders is now
      available from the standard apt repositories

--- debian/scripts/20-install-open-vm-tools.sh
+++ debian/scripts/20-install-open-vm-tools.sh
@@ -4,9 +4,7 @@
 # with VMware based guest VM's
 #
 # Debian based systems no longer need to install the tools bundled with
-# Workstation or Fusion to enable support for shared folders. This used to
-# be necessary due to a bug in the packaging of open-vm-tools. A fix is
-# now in place and is available from the backports repository.
+# Workstation or Fusion to enable support for shared folders.
 #
 # The fuse package is required for shared folder support. However, fuse
 # is only listed as a 'recommended' package in the open-vm-tools package.
@@ -27,39 +25,14 @@
 # Set verbose/quiet output based on env var configured in Packer template
 [[ "${DEBUG}" = true ]] && REDIRECT="/dev/stdout" || REDIRECT="/dev/null"
 
-
 # Packer logging
 echo "Installing open-vm-tools..."
-
-
-# Path to the APT sources file
-APT_SOURCE="/etc/apt/sources.list"
-
-# Create a list of all configured repositories
-REPO_LIST=$(apt-cache policy | grep http: | cut -d' ' -f4 | \
-            cut -d'\' -f1 | sort -u | tr '\n' ' ')
-
-
-# Enable the backports repository if required
-if [ "x$(echo ${REPO_LIST} | grep jessie-backports)" = "x" ]
-then
-    echo "Adding backports repository to APT sources..." > $REDIRECT
-    printf "%s" "\
-
-        # Enable the backports repository
-        deb http://httpredir.debian.org/debian jessie-backports main
-    " | sed 's/^ *//g' >> ${APT_SOURCE}
-fi
 
 # Ensure debconf does not ask any questions
 export DEBIAN_FRONTEND="noninteractive"
 
-# Make APT aware of the new packages available from the backports repo
-apt-get update > ${REDIRECT}
-
 # Install the fuse package and then open-vm-tools from the backports repo
 apt-get -y install fuse > ${REDIRECT}
-apt-get -y -t jessie-backports install open-vm-tools > ${REDIRECT}
-
+apt-get -y install open-vm-tools > ${REDIRECT}
 
 exit 0
