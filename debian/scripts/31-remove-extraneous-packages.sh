8f2aee0b6d3ca0b11e3bf4e5af29590678e58f8c
DanHam <DanHam@users.noreply.github.com>
2018-11-17 00:36:44 +0000

    Debian: Remove extraneous packages

---
+++ debian/scripts/31-remove-extraneous-packages.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Remove extraneous packages
+
+# Set verbose/quiet output based on env var configured in Packer template
+[[ "$DEBUG" = true ]] && redirect="/dev/stdout" || redirect="/dev/null"
+
+# Logging for packer
+echo "Removing extraneous packages..."
+
+packages="avahi-autoipd busybox cpp cpp-6 gcc iamerican installation-report \
+          laptop-detect make task-laptop"
+apt-get --purge autoremove -y ${packages} > ${redirect}
+
+exit 0
