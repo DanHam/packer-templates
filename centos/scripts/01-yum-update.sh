246dc0649fec49b28306c3362ef388f9401a420c
DanHam <DanHam@users.noreply.github.com>
2018-12-12 18:34:17 +0000

    CentOS: Use 'needs-restarting' to check if reboot is required post update
    * Ensure yum-utils is installed to provide the needs-restarting package
    * Fix exit codes
    * Remove linux-firmware package if it is pulled in as a dependancy when
      updating the system

--- centos/scripts/01-yum-update.sh
+++ centos/scripts/01-yum-update.sh
@@ -11,16 +11,6 @@ RPM_GPG_KEY="/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7"
 RPM_GPG_KEY_ID="f4a80eb5"
 # CentOS 7 Official Signing Key Fingerprint
 RPM_GPG_KEY_FINGER="6341 AB27 53D7 8A78 A7C2  7BB1 24C6 A8A7 F4A8 0EB5"
-# Set the location for storing the list of updates from yum check-update
-# using mktemp to avoid potential name conflicts under /tmp
-UPDATE_FILE="$(mktemp -t -q --tmpdir=/tmp updates-XXXXXX.txt)"
-# Create a grep friendly list of packages that require a system reboot
-# after being updated on RHEL/CentOS 7
-# https://access.redhat.com/solutions/27943
-REBOOT_LIST="-e ^kernel -e ^glibc -e ^linux-firmware -e ^systemd -e ^udev"
-# Initialise Reboot flag
-REBOOT=0
-
 
 # Ensure the CentOS RPM GPG key has been imported
 if [ "x$(rpm -qa gpg-pubkey* | grep $RPM_GPG_KEY_ID)" = "x" ]; then
@@ -32,14 +22,13 @@ if [ "x$(rpm -qa gpg-pubkey* | grep $RPM_GPG_KEY_ID)" = "x" ]; then
         rpm --import $RPM_GPG_KEY
     else
         echo "ERROR: CentOS RPM GPG key does not match known fingerprint"
-        exit -1
+        exit 1
     fi
 fi
 
-
-# Check if updates are required and store the list of update packages
-yum check-update > $UPDATE_FILE
-#: yum check-update provides the following exit codes:
+# Check if updates are required
+yum check-update
+# yum check-update provides the following exit codes:
 #     100 => updates are available
 #     1   => an error occurred
 #     0   => no updates
@@ -48,29 +37,32 @@ EXIT_CODE=$?
 if [ $EXIT_CODE == 100 ]; then
     echo "Package updates required. Updating..."
     yum -y update > $REDIRECT
-    # Check the list of updates output by yum check-updates against the
-    # list of packages that require a system restart post update
-    while read LINE
-    do
-        # ...and set to reboot if we have a matching package
-        [[ "x$(echo $LINE | grep $REBOOT_LIST)" != "x" ]] && REBOOT=1
-    done <$UPDATE_FILE
+    # If the kernel was updated then the linux-firmware package may have
+    # been installed as a dependancy. We don't need this so remove
+    if rpm -q linux-firmware >/dev/null; then
+        echo "Removing linux-firmware packer installed with updates"
+        yum -C -y remove linux-firmware
+    fi
 elif [ $EXIT_CODE == 1 ]; then
     echo "An error occurred checking if there are updates for this system"
-    exit -1
+    exit 1
 else
     echo "No packages on the system require updating."
 fi
 
-
 # Remove cached packages and metadata from the yum repository
 yum clean all > $REDIRECT
-# Remove temp files
-[[ -e $UPDATE_FILE ]] && rm -f $UPDATE_FILE
 
-
-# Reboot if required
-if [ $REBOOT == 1 ]; then
+# Reboot if required. The command below returns 1 if a restart is required
+# or 0 otherwise
+# The 'needs-restarting' command is provided by the yum-utils package
+if ! rpm -q yum-utils >/dev/null; then
+    echo "Installing yum-utils to provide the 'needs-restarting' command"
+    yum -C -y install yum-utils > $REDIRECT
+fi
+needs-restarting --reboothint &>/dev/null
+REBOOT=$?
+if [ ${REBOOT} == 1 ]; then
     echo "System restart required post install of updates."
     echo "Stopping the ssh server and then rebooting..."
     # Give time for the output to be logged/sent back to Packer
