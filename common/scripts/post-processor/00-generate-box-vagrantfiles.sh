135699f587f41566535925b5755536d8684ab740
DanHam <DanHam@users.noreply.github.com>
2019-08-05 16:57:03 +0100

    Initial commit of pp script to auto generate box Vagrantfiles

---
+++ common/scripts/post-processor/00-generate-box-vagrantfiles.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+#
+# Auto generate box Vagrantfiles from template variables and user files
+#
+# By default a simple Vagrantfile is created with just the box url.
+#
+# All custom values are set in the Packer template and injected into the
+# build environment as environment variables. Use is also made of the
+# default environment variables set by Packer.
+#
+# Custom Vagrantfile snippets can be incorporated into the file by placing
+# the snippet in a file under the files/template_name directory. The
+# filename should have a suffix comprised of the Packer build type and
+# '.snippet'. Optionally, the filename can include a number before the
+# .snippet suffix to allow ordering of multiple snippets.
+# As an example, to include several snippet in a specific order, for a
+# template named debian-9-x86_64, being built by the vmware-iso builder,
+# and the Vagrantfile base directory set to 'files', the files would need
+# to be created with the following path:
+#
+# files/debian-9-x86_64/Vagrantfile.vmware-iso.00.snippet
+# files/debian-9-x86_64/Vagrantfile.vmware-iso.01.snippet
+#
+# The script will automatically indent all of the snippets contents by two
+# spaces. Any indentation and spacing within the snippet itself will be
+# preserved.
+set -o errexit -o nounset
+
+# Set verbose/quiet output based on setting configured in Packer template
+[ "${DEBUG}" = true ] && redirect="/dev/stdout" || redirect="/dev/null"
+
+# Packer logging
+echo "Generating Vagrantfiles for inclusion in the Vagrant box..."
+
+# Configure required vars using default and custom env vars injected by Packer
+wd="${VAGRANTFILE_BASE_DIR}/${TEMPLATE}"
+vagrantfile="${wd}/${VAGRANTFILE_NAME}"
+vagrantcloud_boxname="${VAGRANT_CLOUD_ORG}/${TEMPLATE}"
+
+# Create the directory if required
+[ ! -d "${wd}" ] && mkdir -p "${wd}"
+
+# Head: Write out file mode or file type hints
+cat <<EOF > "${vagrantfile}"
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+EOF
+
+
+# Write out the head of the Vagrantfile and box name
+echo "Setting box name in Vagrantfile to ${vagrantcloud_boxname}" > ${redirect}
+cat <<EOF >> "${vagrantfile}"
+Vagrant.configure(2) do |config|
+  config.vm.box = '${vagrantcloud_boxname}'
+EOF
+
+
+# Set the ssh user name from setting configured in Packer template. Vagrant
+# will then connect with the correct user name matching the admin user
+# created in the box during the build
+echo "Setting ssh username in Vagrantfile to ${ADMIN_USER}" > ${redirect}
+cat <<EOF >> "${vagrantfile}"
+  config.ssh.username = '${ADMIN_USER}'
+EOF
+
+# Incorporate custom settings and fixes from all relevant Vagrantfile snippets
+while IFS= read -r -d '' snippet; do
+    echo "Incorporating ${snippet} into box Vagrantfile" > ${redirect}
+    echo "" >> "${vagrantfile}" # Insert blank line
+    cat "${snippet}" | sed -E 's/(.*)/  \1/g' >> "${vagrantfile}" # Indent
+done < <(find ${wd} -type f \
+             -name "*${PACKER_BUILDER_TYPE}*.snippet" \
+             -print0 | sort -z)
+
+# Write out the tail of the Vagrantfile
+echo "end" >> ${vagrantfile}
+
+exit 0
